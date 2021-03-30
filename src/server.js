d53d4f4eb92d15bf242a35f0754f4be54d28a8fb
joeldevz <cjzm89@gmail.com>
2021-03-30 23:09:33 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
+import swaggerUI from "swagger-ui-express";
 dotenv.config({ path: __dirname + '/../.env' })
 import app from './app'
 const port = process.env.PORT || 6060
+const swaggerDocument = require('./swagger.json');
+app.use('/v0/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.listen(port, () => {
     console.log(`Server listening on port ${port} http://localhost:${port}`)
 })
